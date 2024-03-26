bc6abafd64d147711e51e1d68835bfb96ecb5e1e
j-kyung99 <apfwnls14@naver.com>
2024-03-26 14:54:42 +0900

    #1.4 주석

---
+++ src/CommentJava.java
@@ -0,0 +1,11 @@
+public class CommentJava {
+    public static void main(String[] args) {
+        // psvm 치면 자동으로 만들어줌
+        System.out.println("hello java1"); //hello java1을 출력합니다.
+        //System.out.println("hello java2");
+        /*
+        System.out.println("hello java3");
+        System.out.println("hello java4");
+        */
+    }
+}
