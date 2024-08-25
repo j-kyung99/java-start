12c91ffb643cbb7ccce91fea50bfbcb8025c2906
j-kyung99 <apfwnls14@naver.com>
2024-08-25 17:19:08 +0900

    #8.3 Scanner-반복 예제

---
+++ src/Scanner/ScannerWhile2.java
@@ -0,0 +1,25 @@
+package Scanner;
+
+import java.util.Scanner;
+
+public class ScannerWhile2 {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("두 수 모두 0을 입력하면 프로그램이 종료됩니다.");
+        while (true) {
+            System.out.print("첫 번째 수 입력:");
+            int num1 = scanner.nextInt();
+
+            System.out.print("두 번째 수 입력:");
+            int num2 = scanner.nextInt();
+
+            if (num1 == 0 && num2 == 0){
+                System.out.println("프로그램을 종료합니다.");
+                break;
+            }
+
+            int sum = num1 + num2;
+            System.out.println("두 숫자의 합: " + sum);
+        }
+    }
+}
